f3328ddbe455490b1efa966d04415514782888ee
danielmoralesp <daniel_moralesp@hotmail.com>
2019-06-18 20:09:13 -0500

    Ticket #4 - Rails Routes

---
+++ my_app/app/controllers/welcome_controller.rb
@@ -0,0 +1,9 @@
+class WelcomeController < ApplicationController
+  def index
+  	@name = "mi nombre"
+  end
+
+  def mi_vista
+  	
+  end
+end
--- my_app/config/routes.rb
+++ my_app/config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+	root 'welcome#index' # el root se declara una sola vez
+  get 'welcome/index'
+  get 'welcome/mi_vista'
+
+  # get 'users/login'
+  # get 'users/sign_up'
+
+
+  # accion, metodo, vista
+
 
 end
