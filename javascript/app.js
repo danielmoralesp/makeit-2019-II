4413a4478a827ef2a776c961415848cdf16628ae
danielmoralesp <daniel_moralesp@hotmail.com>
2019-07-15 19:00:37 -0500

    Ticket #1 - JS

---
+++ javascript/app.js
@@ -0,0 +1,76 @@
+/*
+console.log("Hola Mundo desde editor")
+console.log("segunda linea")
+*/
+
+var nameLastName = "Ana Gonzales"
+
+console.log("entre comillas dobles la persona " + nameLastName + " dijo \"Hola\"")
+console.log(1 + 2)
+console.log(3 * 4 + 5)
+console.log(8 / 2)
+console.log(8 ** 2)
+console.log("1 + 2 es igual a " + (1 + 2))
+
+console.log(5 > 3)
+console.log(1 === "1")
+console.log(1 !== "1")
+
+var name
+console.log(name)
+
+
+
+// 'entre comillas simples'
+
+
+function name_function(name, last_name){
+	return "Hola " + name + " " + last_name + "."
+	console.log("Esta linea no se va a imprimir")
+}
+
+var f1 = name_function("Juana", "Sanchez")
+
+console.log(f1)
+
+//
+
+function bmi(peso, altura){
+	return peso / altura ** 2
+}
+
+bmi = bmi(75, 1.7)
+bmi = Math.floor(bmi)
+bmi = bmi.toFixed(2)
+
+console.log("Tu IMC es: " + bmi)
+
+
+
+if conditional_name
+	// code
+elsif conditional_two
+	// code
+else
+	// code
+end
+
+if (conditional_name){
+	// code
+} else if (conditional_two){
+	// code
+} else {
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
